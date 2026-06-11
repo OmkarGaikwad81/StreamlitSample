9e706e2fd97f3da9f13fdb1ca0215e28df843b98
Omkar Gaikwad <68015658+OmkarGaikwad81@users.noreply.github.com>
2026-06-11 14:02:16 +0530

    Refactor stream.py to improve formatting and structure video file extension chanve

--- stream.py
+++ stream.py
@@ -12,7 +12,7 @@ st.markdown("Currently working as interface analyst at Veradigm(Markdown)")
 st.caption("Looking for new opportunities(Caption)")
 
 st.image("xyz.png")
-st.video("Sample_file.MP4")
+st.video("Sample_file.mp4")
 
 st.checkbox('checkbox')
 st.button('Click button')
